1a3bf64820fd47f4a179ff14e999e4aaa926e40c
S-RR3d-7050 <dahmanishadi@fsb.u-carthage.tn>
2026-06-21 23:14:55 +0100

    Footer Fixes

--- contact.php
+++ contact.php
@@ -1,30 +1,33 @@
 <?php
-$subject = 'New Contact Message'; // Subject of your email
-$to = 'contact@ztc.tn';  //Recipient's E-mail
+$subject = 'New Contact Message';
+$to = 'contact@ztc.tn';
 
-$emailTo = $_POST['email'];
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$msg = $_POST['message'];
+$name = isset($_POST['name']) ? trim($_POST['name']) : '';
+$email = isset($_POST['email']) ? trim($_POST['email']) : '';
+$phone = isset($_POST['phone']) ? trim($_POST['phone']) : '';
+$msg = isset($_POST['message']) ? trim($_POST['message']) : '';
+
+if ($name === '' || $email === '' || $phone === '' || $msg === '' || strpos($email, '@') === false) {
+    echo 'failed';
+    exit;
+}
 
 $email_from = $name . ' ' . '<' . $email . '>';
 
 $headers = "MIME-Version: 1.1";
 $headers .= "Content-type: text/html; charset=iso-8859-1";
-$headers .= "From: " . $name . '<' . $email . '>' . "\r\n"; // Sender's E-mail 
+$headers .= "From: " . $name . '<' . $email . '>' . "\r\n";
 $headers .= "Return-Path:" . "From:" . $email;
 
+$message = '';
 $message .= 'Name : ' . $name . "\n";
 $message .= 'Email : ' . $email . "\n";
 $message .= 'Phone : ' . $phone . "\n";
 $message .= 'Message : ' . $msg;
 
 if (@mail($to, $subject, $message, $email_from)) {
-	// Transfer the value 'sent' to ajax function for showing success message.
-	echo 'sent';
+    echo 'sent';
 } else {
-	// Transfer the value 'failed' to ajax function for showing error message.
-	echo 'failed';
+    echo 'failed';
 }
 ?>
